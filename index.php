5ef06022a1acf57b63ebd74ac78d5d5584d071c4
endhilicious <muhammadaryandi90@gmail.com>
2016-11-17 11:32:39 +0800

    hahaha

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+  /**
+   * ini adalah kelas yang sangat keren
+   */
+  class Indexs extends CI_Controller
+  {
+
+    function __construct()
+    {
+      parent::construct();
+
+    }
+  }
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Judul</title>
+  </head>
+  <body>
+    <p>
+      ini adalah body dari judul dari sebuat perlengkapan manual
+    </p>
+  </body>
+</html>
